f45198a433cf5f8cf7c2e4ac61d1c30e40b36b3a
aresobus <aresobus@gmail.com>
2019-09-10 10:46:53 -0400

    first commit

---
+++ src/js/client/main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+
+
+
+
+
+
+
+/*
+
+REACT
+
+import App from '../components/App'
+
+render(
+	<App />,
+  document.getElementById('root')
+)
+*/
